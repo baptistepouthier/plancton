a59ad7f14a186e9bde94b033c12ecc7d5ef39171
baptistepouthier <39733189+baptistepouthier@users.noreply.github.com>
2018-06-19 11:21:19 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -2,13 +2,11 @@ from keras.models import load_model
 import numpy as np
 import matplotlib.pyplot as plt
 
-model = load_model("C:/Users/Baptiste Pouthier/Documents/partage/Architecture/save")
+model = load_model('/home/bpouthie/test_archi/plancton/save')
 
-X = np.load("C:/Users/Baptiste Pouthier/Documents/partage/Architecture/Kaggle.npy")
+X = np.load('/home/bpouthie/test_archi/plancton/Kaggle.npy')
 
-LABELS = np.load("C:/Users/Baptiste Pouthier/Documents/partage/Architecture/Kaggle_labels.npy")
-
-
+LABELS = np.load('/home/bpouthie/test_archi/plancton/Kaggle_labels.npy')
 
 print("processing...")
 #Y_prob = model.predict(X[123][np.newaxis,:])
@@ -25,37 +23,35 @@ print("processing...")
 #print(Y_classes)
 
 
-#print(LABELS[123][np.axis,:])
+#print(LABELS[123][np.newaxis,:])
 
-conf=np.zeros((121,121)) #confusion matrix
+conf=np.zeros((121,121))
 
 
-for row in range(0,30336):
+for line in range(0,30336):
 
-    Y_prob = model.predict(X[row][np.newaxis, :]) #proba d'appartenance de l'image
+    Y_prob = model.predict(X[line][np.newaxis, :]) #proba d'appartenance de l'image
     Y_classes = Y_prob.argmax(axis=1)  #label PREDIT de l'image via sa proba
-    label=np.where(LABELS[row][np.newaxis,:] == 1)[1][0] #label THEORIQUE de l'image
+    label=np.where(LABELS[line][np.newaxis,:] == 1)[1][0] #label THEORIQUE de l'image
 
     conf[label][Y_classes] += 1 #coeff C[i,j] incremente le nombre d'image en classe j sachant qu'elle appartient reelement en classe i
 
-for row in range (0,121): #normalization : diviser chaque élément d'une ligne par la somme des élments de cette même ligne : pourcentage d'elements i classifiés comme étant de la classe j
+for line in range (0,121): #normalization : diviser chaque élément d'une ligne par la somme des élments de cette même ligne : pourcentage d'elements i classifiés comme étant de la classe j
     for col in range (0,121):
-        conf[row][col]=conf[row][col]/np.sum(conf[row])
+        conf[line][col]=conf[line][col]/np.sum(conf[line])
 
 
 
-#print(conf)
 plt.matshow(conf)
 
 plt.show()
 
 
-#TEST-TEST-TEST-TEST-TEST-TEST-TEST-TEST
 # a=np.array([[1.0, 2.0], [3.0, 4.0]])
 # print(a)
 #
-# for row in range (0,2): #normalization
+# for line in range (0,2): #normalization
 #      for col in range (0,2):
-#         a[row][col]=(a[row][col])/np.sum(a[row])
+#         a[line][col]=(a[line][col])/np.sum(a[line])
 #
 # print(a)
