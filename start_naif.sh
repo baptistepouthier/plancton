aa1df9c64e3111e240d2c97a66bd60a4557a3574
baptistepouthier <39733189+baptistepouthier@users.noreply.github.com>
2018-07-03 17:07:18 +0200

    Update start_naif.sh

--- start_naif.sh
+++ start_naif.sh
@@ -8,9 +8,7 @@ cp ../gen_features_* .
 
 module load cuda/8.0
 
-
 module load cudnn/5.1-cuda-8.0
-
 
 #export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp34-cp34m-linux_x86_64.whl
 
@@ -18,13 +16,8 @@ export TF_BINARY_URL=https://pypi.python.org/packages/d1/ac/4cbd5884ec518eb1064e
 
 #pip3 install --upgrade pip
 
-
 pip3 install $TF_BINARY_URL
 
 pip3 install keras h5py pillow
 
-
-
-#pip3 install $TF_BINARY_URL
-cd /home/bpouthie/Documents/partage/confusion
 python3 test.py
