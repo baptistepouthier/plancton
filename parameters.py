470b108c97b7543cb36fd1ce76d4613a2b03bbda
baptistepouthier <39733189+baptistepouthier@users.noreply.github.com>
2018-08-08 15:08:45 +0200

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,10 @@
+COMMON_PATH = '/home/bpouthie/Documents/partage_2'
+# COMMON_PATH = 'D:/Users/Baptiste Pouthier/Documents/partage_2'
+root_dir = COMMON_PATH+'/clusters/iteration_0/cluster_n0'
+max_conf = 0.5
+img_size = 95
+batch_size = 64
+run_from_zero = True
+show_network = False
+show_conf_matrix = False
+
