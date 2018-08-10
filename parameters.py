55d78e9d0f3b393561ca4350342b5e5ae558bc5b
baptistepouthier <39733189+baptistepouthier@users.noreply.github.com>
2018-08-10 10:28:25 +0200

    Add files via upload

--- parameters.py
+++ parameters.py
@@ -4,7 +4,7 @@ root_dir = COMMON_PATH+'/clusters/iteration_0/cluster_n0'
 max_conf = 0.5
 img_size = 95
 batch_size = 64
-run_from_zero = True
+run_from_zero = False
 show_network = False
 show_conf_matrix = False
 
